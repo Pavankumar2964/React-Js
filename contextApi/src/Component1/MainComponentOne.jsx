8a244ba5ccb9cfb3eca29d8331634c73d7b5b8a7
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-19 21:37:31 +0530

    ContextAPI

--- contextApi/src/Component1/MainComponentOne.jsx
+++ contextApi/src/Component1/MainComponentOne.jsx
@@ -7,27 +7,29 @@ import ChildComponentOne from "./ChildComponentOne";
 
 
 export default class MainComponentOne extends Component{
+    
     constructor(props){
         super(props);
         this.state={
             message:"Main Component Message sending to parent Component",
             person:{
-                name:"Pavan",
+                name:"Pavan"},
                 student:{
                     Name:"Pavan",
                     Id:"22PK29",
                     Email:"Pavankumar29ctr@gamil.com",
                     Age:"21",
                 }
-            },
+            
         };
     }
+    
     render(){
         return(
             <div>
                 <MesssageAccessProvider value={this.state}
                 >
-                    
+                   
                     <ChildComponentOne/>
                 </MesssageAccessProvider>
             </div>
