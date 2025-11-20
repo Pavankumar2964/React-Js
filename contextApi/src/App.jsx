65c781526596da85e9637a5d28a9f783e359eb62
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-20 15:45:06 +0530

    ContextAPI

--- contextApi/src/App.jsx
+++ contextApi/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MainComponentOne from './Component1/MainComponentOne'
+import MainComponentTwo from './Component2/MainComponentTwo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,8 @@ function App() {
   return (
     <>
       <div>
-        <MainComponentOne/>
+       
+        <MainComponentTwo/>
       </div>
    
     </>
