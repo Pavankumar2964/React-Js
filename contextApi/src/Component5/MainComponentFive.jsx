db176d7c54fd100a163782ec5b0a856931a56a2b
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-24 10:22:03 +0530

    ContextAPI

--- contextApi/src/Component5/MainComponentFive.jsx
+++ contextApi/src/Component5/MainComponentFive.jsx
@@ -1,1 +1,26 @@
+import {Component, createContext} from "react";
+import { MessageAccessProvider } from "./ContextCompFive";
+import ChildComponentFive from "./ChildComponentFive";
 
+
+export default class MainComponentFive extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            Students:[
+                {Name:"Pavan",age:"21",Id:"22pk29"},
+                {Name:"Kumar",age:"21",Id:"22kr22"},
+                {Name:"Yaswant",age:"21",Id:"22yk23"},
+            ]
+        }
+    }
+    render(){
+        return(
+            <div>
+                <MessageAccessProvider value={this.state}>
+                    <ChildComponentFive/>
+                </MessageAccessProvider>
+            </div>
+        )
+    }
+}
