cc945af596a67071c68f2b937fb49e5c00de6e5a
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-24 21:18:17 +0530

    ContextAPI

---
+++ contextApi/src/Component6/ContextSix.jsx
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+const messageAccess=createContext();
+export const MessageAccessProvider=messageAccess.Provider;
+export const MessageAccessConsumer=messageAccess.Consumer;
