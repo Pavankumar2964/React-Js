23f052523d5207a81c482a21e6df0d3d3091cfe6
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-24 10:21:55 +0530

    ContextAPI

--- contextApi/src/Component5/ChildComponentFive.jsx
+++ contextApi/src/Component5/ChildComponentFive.jsx
@@ -1,1 +1,35 @@
+import { Component } from "react";
+import { MessageAccessConsumer } from "./ContextCompFive";
 
+
+// ChildComponentFive.jsx (Corrected)
+import { Component } from "react";
+import { MessageAccessConsumer } from "./ContextCompFive";
+
+
+export default class ChildComponentFive extends Component{
+    render(){
+        return(
+            <div>
+                <MessageAccessConsumer>{(value)=>{
+                     console.log(value) 
+                    return (
+                        <div>
+                           
+                            {value.Students.map((result)=>{
+                                return(
+                                    <ul key={result.Id}> 
+                                        {Object.values(result).map((val, index)=>{
+                                          
+                                            return <li key={index}>{val}</li>
+                                        })}
+                                    </ul>
+                                )
+                            })}
+                        </div>
+                    )
+                }}</MessageAccessConsumer>
+            </div>
+        )
+    }
+}
