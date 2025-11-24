58445f272a1be11eae99629c8a8cdd1b7270e120
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-24 16:38:04 +0530

    ContextAPI

--- contextApi/src/Component5/ChildComponentFive.jsx
+++ contextApi/src/Component5/ChildComponentFive.jsx
@@ -1,35 +1,6 @@
-import { Component } from "react";
-import { MessageAccessConsumer } from "./ContextCompFive";
-
-
-// ChildComponentFive.jsx (Corrected)
-import { Component } from "react";
-import { MessageAccessConsumer } from "./ContextCompFive";
+import {Component}from "react"
 
 
 export default class ChildComponentFive extends Component{
-    render(){
-        return(
-            <div>
-                <MessageAccessConsumer>{(value)=>{
-                     console.log(value) 
-                    return (
-                        <div>
-                           
-                            {value.Students.map((result)=>{
-                                return(
-                                    <ul key={result.Id}> 
-                                        {Object.values(result).map((val, index)=>{
-                                          
-                                            return <li key={index}>{val}</li>
-                                        })}
-                                    </ul>
-                                )
-                            })}
-                        </div>
-                    )
-                }}</MessageAccessConsumer>
-            </div>
-        )
-    }
+
 }
--- contextApi/src/Component5/MainComponentFive.jsx
+++ contextApi/src/Component5/MainComponentFive.jsx
@@ -1,24 +1,21 @@
-import {Component, createContext} from "react";
+import { Component } from "react";
 import { MessageAccessProvider } from "./ContextCompFive";
-import ChildComponentFive from "./ChildComponentFive";
 
 
 export default class MainComponentFive extends Component{
     constructor(props){
         super(props);
-        this.state={
-            Students:[
-                {Name:"Pavan",age:"21",Id:"22pk29"},
-                {Name:"Kumar",age:"21",Id:"22kr22"},
-                {Name:"Yaswant",age:"21",Id:"22yk23"},
-            ]
-        }
+        this.state={Student:[
+            {Name:"Pavan",age:"21",Id:"22pk29"},
+            {Name:"Koushik",age:"6",Id:"22kk18"},
+            {Name:"Advik",age:"3",Id:'22ad09'},
+        ]}
     }
     render(){
         return(
             <div>
                 <MessageAccessProvider value={this.state}>
-                    <ChildComponentFive/>
+                    
                 </MessageAccessProvider>
             </div>
         )
