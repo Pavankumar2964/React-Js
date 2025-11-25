f08ed490b3063d7ccede2eece6d2c4ff059bb623
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-25 17:56:06 +0530

    redux

--- redux-demo/src/App.jsx
+++ redux-demo/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux';
-import { store } from './store'
+
 import { decrement, increment, reset } from './CounterSlice';
 
 function App() {
