db7c3fc5e107d78e1cc2f82b083d20bfd949ed00
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-25 21:02:21 +0530

    Redux

---
+++ reduxOne/src/counterSliceOne.js
@@ -0,0 +1,13 @@
+import {createSlice} from "@reduxjs/toolkit";
+const initialState={
+    count:0,
+}
+export const counterSliceOne=createSlice({
+    name:"counter",
+    reducers:{
+
+    },
+    initialState:initialState,
+
+})
+export default counterSliceOne.reducer;
