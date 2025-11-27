f8d1786f3d77d8a0edd9e593f3fb008216e8c4f0
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-27 11:47:33 +0530

    redux

--- reduxOne/src/App.jsx
+++ reduxOne/src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrementcount, incrementcount, reset } from './counterSliceOne'
 
 function App() {
-  const [count, setCount] = useState(0)
+const counter=useSelector((state)=>state.counter.value)
+const dispatch=useDispatch()
+console.log(counter)
 
   return (
     <>
-     
+     <h2>counter value:{counter}</h2>
+     <button onClick={()=>{dispatch(incrementcount())}}>Increment</button>
+      <button onClick={()=>{dispatch(decrementcount())}}>Decrement</button>
+       <button onClick={()=>{dispatch(reset())}}>Reset</button>
     </>
   )
 }
