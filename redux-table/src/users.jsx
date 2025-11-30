5c4391da81ddbe224b98252f8b64a0a7cc6f05c8
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-30 13:28:20 +0530

    Redux-table

---
+++ redux-table/src/users.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {useSelector} from "react-redux";
+import UsersTable from "./UsersTable";
+
+const Users=()=>{
+    const userDetails = useSelector((state)=>state.userInfo.usersArray);
+console.log(userDetails);
+return(
+    <div>
+        <h2>Welcome to UsersFromTable</h2>
+<UsersTable displayUsers={userDetails} />
+    </div>
+)
+}
+export default Users;
