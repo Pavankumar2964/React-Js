f76a12020facfa5511c3ec18ff7ee47e2fb7ea62
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-12-04 22:12:36 +0530

    Reduxform

---
+++ reduxformfour/src/UserSlice.js
@@ -0,0 +1,65 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { act } from "react";
+
+const initialUsers={
+     usersArry: [
+    {
+      id: 1,
+      name: "Arun Kumar",
+      email: "arun.kumar@example.com",
+      role: "admin",
+      age: 32,
+      city: "Bangalore",
+    },
+    {
+      id: 2,
+      name: "Priya Sharma",
+      email: "priya.sharma@example.com",
+      role: "user",
+      age: 27,
+      city: "Hyderabad",
+    },
+    {
+      id: 3,
+      name: "Rohit Reddy",
+      email: "rohit.reddy@example.com",
+      role: "editor",
+      age: 29,
+      city: "Chennai",
+    },
+    {
+      id: 4,
+      name: "Lakshmi Devi",
+      email: "lakshmi.devi@example.com",
+      role: "user",
+      age: 24,
+      city: "Vijayawada",
+    },
+    {
+      id: 5,
+      name: "Manoj Varma",
+      email: "manoj.varma@example.com",
+      role: "admin",
+      age: 34,
+      city: "Mumbai",
+    },
+  ],
+};
+export const UserSlice= createSlice({
+    name:"users",
+    initialState:initialUsers,
+    reducers:{
+       createAction:(state,action)=>{
+        action.payload.id=state.usersArry.length+1;
+       } ,
+       deleteUserAction:(state,action)=>{
+        console.log(action.payload);
+        state.usersArry=state.usersArry.filter((user)=>{
+            return user.id !==action.payload.id;
+        });
+       },
+    }
+
+});
+export const{createAction,deleteUserAction}=UserSlice.actions;
+export default UserSlice.reducer;
