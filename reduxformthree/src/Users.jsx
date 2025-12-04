62f3cc33da00be9e4ca1a00c7d1ded9223008f00
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-12-04 12:36:59 +0530

    Reduxform

--- reduxformthree/src/Users.jsx
+++ reduxformthree/src/Users.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import Userstable from "./Userstable";
 import UserForm from "./UsersForm";
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
+import { createUserAction, deleteUserAction } from "./userSlice";
 
 const Users = () => {
     // 1. Get the users array from the Redux store
-    // Matches the reducer name "UserInfo" in store.js and "usersArry" in UserSlice.js
     const users = useSelector((state) => state.UserInfo.usersArry);
+    const dispatch=useDispatch();
+    
+    // FIX 1: Changed userDetails to users
+    console.log(users); 
 
+    const deleteUser=(user)=>{
+        console.log(user);
+        dispatch(deleteUserAction(user));
+    };
+    const addUser=(user)=>{
+        dispatch(createUserAction(user));
+    };
     return (
         <div>
             <h2>Welcome to Users</h2>
             <hr />
-            {/* 2. Pass the actual array variable, not the component name */}
-            <Userstable users={users} />
+            {/* FIX 2: Changed userDetails to users */}
+            <Userstable users={users} deleteUser={deleteUser} />
             <br />
             <hr />
-            <UserForm />
+            <UserForm addUser={addUser} />
         </div>
     )
 }
