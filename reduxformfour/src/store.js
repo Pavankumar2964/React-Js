926c7b259a78bac9fc5e29df9538b3766df5a19a
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-12-05 21:52:38 +0530

    ReduxForm

--- reduxformfour/src/store.js
+++ reduxformfour/src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import  UserSlice  from "./UserSlice";
  export const store = configureStore({
     reducer:{
-        
+      usersInfo:UserSlice  
     }
  })
