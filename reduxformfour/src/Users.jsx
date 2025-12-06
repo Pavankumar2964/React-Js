268331815fd890cdec83d628b675ccc902fc5f0d
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-12-06 19:30:26 +0530

    Redux

--- reduxformfour/src/Users.jsx
+++ reduxformfour/src/Users.jsx
@@ -1,28 +1,28 @@
-import React,{ useState} from "react";
-import {useDispatch,useSelector} from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { createUserAction, deleteUserAction } from "./UserSlice";
 import UsersForm from "./UsersForm";
 import UsersTable from "./UsersTable";
 
 
-const Users=()=>{
-    const usersDetails = useSelector((state)=>state.usersInfo.usersArry);
-    const dispatch=useDispatch();
+const Users = () => {
+    const usersDetails = useSelector((state) => state.usersInfo.usersArry);
+    const dispatch = useDispatch();
     console.log(usersDetails);
 
-    const deleteUser=(user)=>{
+    const deleteUser = (user) => {
         console.log(user);
         dispatch(deleteUserAction(user));
     };
 
-    const addUser=(user)=>{
+    const addUser = (user) => {
         dispatch(createUserAction(user));
     };
-    return(
+    return (
         <div>
             <h2>Welcome to Users</h2>
-            <UsersForm addUser={addUser}/>
-            <UsersTable users={usersDetails} deleteUser={deleteUser}/>
+            <UsersForm addUser={addUser} />
+            <UsersTable users={usersDetails} deleteUser={deleteUser} />
         </div>
     )
 }
