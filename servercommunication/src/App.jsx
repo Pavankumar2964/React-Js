f91bb8999aaf89e732bb1b6248ae22b8bb545fe1
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-12-16 20:33:52 +0530

    ServerCommunication

--- servercommunication/src/App.jsx
+++ servercommunication/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { User } from './Users'
+import { UserOne } from './UserOne'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   <User/>
+   {/* <User/> */}
+   <UserOne/>
     </>
   )
 }
