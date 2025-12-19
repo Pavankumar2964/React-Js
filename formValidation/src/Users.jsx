19fe8b08501526407d9ee973e82062017b84f129
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-12-19 20:19:01 +0530

    FormValidation

--- formValidation/src/Users.jsx
+++ formValidation/src/Users.jsx
@@ -1,39 +1,39 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 
-export const Users =()=>{
-    const[usersValues,setUsersValues] =useState({
-        fName:"",
-        lName:"",
+export const Users = () => {
+    const [usersValues, setUsersValues] = useState({
+        fName: "",
+        lName: "",
     });
 
-    const handleChange=(e)=>{
-        const newUsersValues={...usersValues};
-        newUsersValues[e.target.name]=e.target.value;
+    const handleChange = (e) => {
+        const newUsersValues = { ...usersValues };
+        newUsersValues[e.target.name] = e.target.value;
         setUsersValues(newUsersValues);
         console.log(e)
     };
-    useEffect(()=>{
-        if(usersValues.fName.length>=8 && usersValues.lName.length>=6) {
-            document.querySelector("button").removeAttribute("disabled",true);
+    useEffect(() => {
+        if (usersValues.fName.length >= 8 && usersValues.lName.length >= 6) {
+            document.querySelector("button").removeAttribute("disabled", true);
         } else {
-            document.querySelector("button").setAttribute("disabled",true)
+            document.querySelector("button").setAttribute("disabled", true)
         }
         [usersValues];
     })
-    return(
+    return (
         <div>
             <h2>Welcome to User Component</h2>
-            <hr/>
+            <hr />
             <form>
                 <label htmlFor="">First Name</label>
                 <input type="text" name="fName" value={usersValues.fName}
-                onChange={handleChange}/>
-                <br/>
-                 <label htmlFor="">Last Name</label>
+                    onChange={handleChange} />
+                <br />
+                <label htmlFor="">Last Name</label>
                 <input type="text" name="lName" value={usersValues.lName}
-                onChange={handleChange}/>
-                <br/>{" "}
-                <br/>
+                    onChange={handleChange} />
+                <br />{" "}
+                <br />
                 <button disabled>Add User</button>
             </form>
         </div>
