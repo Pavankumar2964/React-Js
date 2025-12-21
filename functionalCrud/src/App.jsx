7e4563bb8c02917faa49f904baac9db624bc2830
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-12-21 21:37:40 +0530

    FunctionalCrud

--- functionalCrud/src/App.jsx
+++ functionalCrud/src/App.jsx
@@ -2,32 +2,80 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Studentform } from './Studentsform'
+import { Studentstable } from './Studentstable'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [student,setStudent] =useState({
+    fName:"",
+    lName:"",
+    email:"",
+  });
+
+  const [studentsArray,setStudentsArray] = useState([
+    {fName:"Pavan",lName:"kumar",email:"pavankumar@gmail.com" },
+    {fName:"Pavan",lName:"kumar",email:"pavankumar@gmail.com" },
+    {fName:"Pavan",lName:"kumar",email:"pavankumar@gmail.com" },
+  ]);
+
+  const [editIndex, setEditIndex]=useState(null);
+  const handleChange=(e)=>{
+    console.log(e);
+    const newStudent={...student};
+    newStudent[e.target.name]=e.target.value;
+    setStudent(newStudent);
+  };
+  const handleSubmit=()=>{
+    const newStudentArray =[...studentsArray];
+    newStudentArray.push(student);
+    setStudentsArray(newStudentArray);
+    clearForm();
+  };
+
+  const clearForm=()=>{
+    setStudent({
+      fName:"",
+      lName:"",
+      email:"",
+    });
+  };
+
+  const handleDeleteStudent=(i)=>{
+    const newStudents=[...studentsArray];
+    newStudents.splice(i,1);
+    setStudentsArray(newStudents);
+  };
+
+  const handleEditStudent=(i)=>{
+    setStudent(studentsArray[i]);
+    setEditIndex(i);
+  };
+
+  const handleUpdateStudent=()=>{
+    const newStudents=[...studentsArray];
+    newStudents[editIndex]=student;
+    setStudentsArray(newStudents);
+    clearForm();
+    setEditIndex(null);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  <Studentform 
+  student={student}
+  handleChange={handleChange}
+  handleSubmit={handleSubmit}
+  handleUpdateStudent={handleUpdateStudent}
+  editIndex={editIndex}
+  />
+  <hr/>
+  <Studentstable
+   studentsArray={studentsArray}
+   handleDeleteStudent={handleDeleteStudent}
+   handleEditStudent={handleEditStudent}
+   />
+  
     </>
   )
 }
