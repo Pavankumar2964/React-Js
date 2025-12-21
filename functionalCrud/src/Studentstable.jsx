25456290a1c222a1022bccec9c62f58a988d4eb8
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-12-21 21:38:32 +0530

    FunctionalCrud

--- functionalCrud/src/Studentstable.jsx
+++ functionalCrud/src/Studentstable.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-export const Studentstable=({
-    studentsArray,handleDeleteStudent,handleEditStudent
-})=>{
-    return(
+export const Studentstable = ({
+    studentsArray, handleDeleteStudent, handleEditStudent
+}) => {
+    return (
         <div>
             <h2>Welcome to Studentstable component</h2>
             <table border={1}>
@@ -14,24 +14,24 @@ export const Studentstable=({
                         <th>Edit</th>
                         <th>Delete</th>
                     </tr>
-                  </thead>
-                  <tbody>
-                    {studentsArray.map((std,i)=>{
-return(
-    <tr key={i}>
-        <td>{std.fName}</td>
-        <td>{std.lName}</td>
-        <td>{std.email}</td>
-        <td><button onClick={()=>{
-            handleEditStudent(i)
-        }}>Edit</button></td>
-        <td><button onClick={()=>{
-handleDeleteStudent()
-        }}>Delete</button></td>
-    </tr>
-)
+                </thead>
+                <tbody>
+                    {studentsArray.map((std, i) => {
+                        return (
+                            <tr key={i}>
+                                <td>{std.fName}</td>
+                                <td>{std.lName}</td>
+                                <td>{std.email}</td>
+                                <td><button onClick={() => {
+                                    handleEditStudent(i)
+                                }}>Edit</button></td>
+                                <td><button onClick={() => {
+                                    handleDeleteStudent()
+                                }}>Delete</button></td>
+                            </tr>
+                        )
                     })}
-                  </tbody>
+                </tbody>
             </table>
         </div>
     )
