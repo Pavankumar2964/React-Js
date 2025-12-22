7bc22e691ccfea2fda6a9369b757559a3fe1aba6
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-12-22 20:33:21 +0530

    Redux

--- redux/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./counterSlice"; // Import the default export
-
-export const store = configureStore({
-    reducer: {
-        counter: counterReducer, // Key must match the selector in App.jsx
-    },
-});
